1722941094bb809690db7626cce19cbc359f5ceb
KUN-NT <332271028@qq.com>
2020-03-01 10:10:24 +0800

    update config

--- lemall/vue.config.js
+++ lemall/vue.config.js
@@ -1,24 +1,24 @@
-module.exports={
+module.exports = {
     //前端server和接口代理
-    devServer:{
-        host:'localhost',
-        port:9090,
-        proxy:{
-            '/api/*':{
-                target:'http://localhost:3000'
+    devServer: {
+        host: 'localhost',
+        port: 9090,
+        proxy: {
+            '/api/*': {
+                target: 'http://localhost:3000'
             }
         }
     },
     //项目的基本路径
-    publicPath:'/',
+    publicPath: '/',
     //项目的生成目录
-    outputDir:'dist',
+    outputDir: 'dist',
     //修改index.html的路径
-    indexPath:'demo.html',
+    indexPath: 'index.html',
     //为静态资源设置hash值，这样重复请求资源都会从服务器获取新的数据 而不是读取缓存数据
-    filenameHashing:true,
+    filenameHashing: true,
     //eslint是否开启
-    lintOnSave:false,
+    lintOnSave: false,
     //是否启用sourceMap
-    productionSourceMap:false
+    productionSourceMap: false
 }
