1d5417e2e26283b1434bc98bcbe1fef2c118dff2
KUN-NT <332271028@qq.com>
2020-07-02 22:20:42 +0800

    update Vue-Cli to 4.x

---
+++ lemall/vue.config.js
@@ -0,0 +1,24 @@
+module.exports={
+    //前端server和接口代理
+    devServer:{
+        host:'localhost',
+        port:9090,
+        proxy:{
+            '/api/*':{
+                target:'http://localhost:3000'
+            }
+        }
+    },
+    //项目的基本路径
+    publicPath:'/',
+    //项目的生成目录
+    outputDir:'dist',
+    //修改index.html的路径
+    indexPath:'demo.html',
+    //为静态资源设置hash值，这样重复请求资源都会从服务器获取新的数据 而不是读取缓存数据
+    filenameHashing:true,
+    //eslint是否开启
+    lintOnSave:false,
+    //是否启用sourceMap
+    productionSourceMap:false
+}
